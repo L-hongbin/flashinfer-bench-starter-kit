981bf5caffe86df55462f03f2c284028a17eaf70
luomao <romaosir88@gmail.com>
2026-03-19 13:16:54 +0000

    add eval script

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,5 +1,5 @@
 #export FIB_DATASET_PATH=/home/hanling/code_agent/dataset/mlsys26-contest/
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=4
 export FIB_DATASET_PATH=/home/lihongbin/dataset/mini-mlsys26-contest/
 cd /home/lihongbin/flashinfer-bench-starter-kit/scripts
 python run_local.py
